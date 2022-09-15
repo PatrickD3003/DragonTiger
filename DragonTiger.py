b0847eb7b8a588195f04a6733a52bd54ead682e1
AD <AD@LLs-MacBook-Air.local>
2022-09-15 15:22:05 +0700

    apus beberapa file

--- DragonTiger.py
+++ DragonTiger.py
@@ -1,6 +1,8 @@
+
 import random
 import math
 import itertools
+from traceback import StackSummary
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
@@ -10,6 +12,7 @@ B = list(itertools.combinations_with_replacement(A,5))
 lawan = [0,0,1,1,1,0,0,0,1,0,1,0,0,0,0,2,0,1,0,0,0,0,0,1,1,0,0,2,0,0,0,0,0,0,1,0,1,1,1,1,1,1,0,1,0,2,0,1,1,1,1,0,0,0,2,1,1,0,1,0,1,1,1,1,0,2,0,1,0,1,0,0,0,0,1,0,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,1,2,0,2,1,0,1,1,1,0,0,0,0,1,1,0,1,0,0,0,0,0,1,0,0,2,0,1,0,1,0,1,1,1,0,0,1,0,2,1,0,1,1,1,1,1,1,1,1,0,0,0,1]
 lawanRandom = []
 summary = [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]]
+
 def countGenerator():
     global A,AT
     for i in B:
@@ -26,18 +29,27 @@ def compare(siapa):
     j = 0 
     k = 0   
     match = 0
+    gagal = 0
     while True:
         
         if i > len(AT)-1:
             break
-        if ( k > len(siapa)-1 ) or ( j > 4 ) :
+        #if finish one combination
+        if ( k > len(siapa)-1 ) :
                 #print(f"kombinasi ke {i+1}{AT[i]}, banyak match {match}, kelar di {k} WIN THE GAME")
-                summary[i].append([match,k])
+                summary[i].append([match,gagal])
                 i += 1
                 j = 0
                 k = 0
                 match = 0
+                gagal = 0
                 continue
+        #kalo kalah, tetep lanjut
+        elif j > 4:
+            j = 0
+            k += 1
+            gagal += 1
+            continue
         
         #kalo match
         if AT[i][j] == siapa[k]:
@@ -61,7 +73,7 @@ def enemyGenerator():
 
 
 def summ():
-    print("--------------SUMMARY:left =  total match, right = lokasi kelar----------------")
+    print("--------------SUMMARY:left =  total match, right =  banyak gagal----------------")
     for i,j in enumerate(summary):
         print(f"kombinasi nomor {i+1} {AT[i]} : {j}")
 
@@ -70,16 +82,15 @@ def datFrame():
     # length max l = len(summary)-1 = 31
     # length max m = len(summary[0])-1
     MTCH = []
-    KLR = []
-
+    GGL = []
     for l in range(len(summary)):
             for m in range(len(summary[0])):
                 MTCH.append(summary[l][m][0])
-                KLR.append(summary[l][m][1])
+                GGL.append(summary[l][m][1])
                 if m == len(summary[0])-1:
                     dats = {
                         "match" : MTCH,
-                        "kelar" : KLR
+                        "gagal" : GGL
                     }
                     df = pd.DataFrame(dats)
                     DeValue = np.std(MTCH)
@@ -87,7 +98,7 @@ def datFrame():
                     print(f'kombinasi ke {l+1} , {AT[l]} Standard Deviation Value : {DeValue:.2f}, mean {meValue:.2f}')
                     print(df)
                     MTCH = []
-                    KLR = []
+                    GGL = []
                     continue
                 else:
                     continue
@@ -95,22 +106,21 @@ def datFrame():
 def matplot():
     global summary
     MTCH_plt = []
-    KLR_plt = []
+    GGL_plt = []
     #add text
     for l in range(len(summary)):
         for m in range(len(summary[0])):
             MTCH_plt.append(summary[l][m][0])
-            KLR_plt.append(summary[l][m][1])
+            GGL_plt.append(summary[l][m][1])
             if m == len(summary[0])-1:
-
-                plt.plot(MTCH_plt,'ro')
+                plt.plot(GGL_plt,'ro')
                 plt.xlabel("nomer")
-                plt.ylabel("TOTAL MATCH")
+                plt.ylabel("banyak fail")
                 plt.title(f"kombinasi ke {l+1}")
                 plt.show()
 
+                GGL_plt = []
                 MTCH_plt = []
-                KLR_plt = []
                 continue
             else:
                 continue
@@ -121,11 +131,11 @@ countGenerator()
 #print(lawan)
 compare(lawan)
 
-for engen in range(10):
+
+for engen in range(20):
     enemyGenerator()
     #print(lawanRandom)
     compare(lawanRandom)
-
 
 
 
